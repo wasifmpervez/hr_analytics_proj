d36ab2956fac3e1ae2389c8d7b999c716d085793
wasifmpervez <68664894+wasifmpervez@users.noreply.github.com>
2024-08-12 02:18:26 -0400

    upload raw data 3 and setup file

---
+++ setup.sql
@@ -0,0 +1,27 @@
+-- CREATE SCHEMA `hr_analytics_proj` ;
+
+-- CREATE TABLE `hr_analytics_proj`.`employees` (
+-- 	`age` INT(3) NULL,
+--     `attrition_yn` VARCHAR(3) NULL,
+--     `business_travel_freq` VARCHAR(50) NULL,
+--     `department` VARCHAR(50) NULL,
+--     `distance_from_home` INT(3) NULL,
+--     `education` VARCHAR(1) NULL,
+--     `education_field` VARCHAR(50) NULL,
+--     `employee_count` INT(1) NULL,
+--     `employee_id` INT(4) NOT NULL AUTO_INCREMENT,
+--     `gender` VARCHAR(50) NULL,
+--     
+
+-- )
+
+-- E X A M P L E
+-- `id` INT(11) NOT NULL AUTO_INCREMENT,
+--   `Date` DATE NULL,
+--   `Band` VARCHAR(100) NULL,
+--   `ConcertName` VARCHAR(100) NULL,
+--   `Country` VARCHAR(50) NULL,
+--   `City` VARCHAR(50) NULL,
+--   `Location` VARCHAR(100) NULL,
+--   `LocationAddress` VARCHAR(100) NULL,
+--   PRIMARY KEY (`id`));
